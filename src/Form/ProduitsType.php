5f34234bf27c702e1389921d05ee9448f11b3732
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-30 15:30:21 +0100

    modif

--- src/Form/ProduitsType.php
+++ src/Form/ProduitsType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Produits;
+use App\Entity\Categories;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class ProduitsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -15,7 +18,25 @@ class ProduitsType extends AbstractType
             ->add('nom')
             ->add('slug')
             ->add('description')
-            ->add('categorie')
+ 
+            ->add('categorie', EntityType::class, [
+                'class' => Categories::class,
+                'label' => false,
+                'choice_label' => 'nom',
+                'empty_data'=>'',
+                'required'=>false,
+         
+            ])
+            ->add('files',FileType::class,[
+                'label'=> 'Photos',
+                'multiple' => true,
+                'mapped'=> false,
+                'required'=> false,
+                
+        
+            
+            ])
+
         ;
     }
 
