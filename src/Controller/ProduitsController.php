52c2ec5d63746be579ffac8b7fac1474b0444f26
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-30 15:45:10 +0100

    modif

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -44,7 +44,7 @@ class ProduitsController extends AbstractController
                 // Je stocke le document dans la BDD (nom du fichier)
                 $file= new Files();
                 $file->setPath($fichier);
-                $produit->setFile($file);
+                $produit->setFiles($file);
 
             }
 
