84e90abf07d6cae31e8b9c3d801c61614a24255d
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-10 18:36:45 +0100

    modif

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\ProduitsRepository;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ProduitsRepository $produitsRepository): Response
     {
+        $produits = $produitsRepository->findAll();
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'produit'=> $produits
         ]);
     }
 }
