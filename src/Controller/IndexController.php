1f2c5e03b5627dfa8d5cee6e4366c70ee898de1e
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-11 00:36:27 +0100

    modif

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produits;
 use App\Repository\ProduitsRepository;
 class IndexController extends AbstractController
 {
@@ -16,6 +17,14 @@ class IndexController extends AbstractController
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'produits'=> $produits
+        ]);
+    }
+
+    #[Route('/détails/{id}', name: 'app_produit_details', methods: ['GET'])]
+    public function show(Produits $produit): Response
+    {
+        return $this->render('produits/show.html.twig', [
+            'produit' => $produit,
         ]);
     }
 }
