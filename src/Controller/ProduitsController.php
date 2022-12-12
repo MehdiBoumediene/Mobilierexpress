91b2ae7015f780859bbef81846c8ce808f3c1b4a
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-12 15:03:40 +0100

    modif

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -9,6 +9,7 @@ use App\Repository\ProduitsRepository;
 use App\Repository\FilesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
