d005ec47211f18b26875745518136dcffe165609
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-12 15:29:31 +0100

    modif

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -15,7 +15,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\File\File;
 #[Route('/admin/produits')]
 class ProduitsController extends AbstractController
 {
