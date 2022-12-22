5b1c3fa80697f1041c17ae5c71300d0bc32eeba7
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-22 14:22:42 +0100

    update

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
+use Symfony\Bundle\SwiftmailerBundle;
 
 #[Route('/commandes')]
 class CommandesController extends AbstractController
