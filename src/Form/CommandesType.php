7c94ea6e5320ad4c8a5285db0b97f0ab28bc7f0a
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-22 13:08:10 +0100

    update

--- src/Form/CommandesType.php
+++ src/Form/CommandesType.php
@@ -12,13 +12,69 @@ class CommandesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('telephone')
-            ->add('etage')
-            ->add('couleur')
-            ->add('dimentions')
-            ->add('Email')
-            ->add('produit')
+            ->add('nom',TextType::class, [
+                'label' => false
+                ])
+            ->add('telephone',TextType::class, [
+                'label' => false
+                ])
+           
+            ->add('etage',ChoiceType::class, [
+                'choices' => [
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    "4" => "4",
+                    '5' => '5',
+                    '6' => '6',
+                    '7' => '7',
+                    'plus' => 'plus',
+                    
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'required' => false,
+                'label' => false
+            ])
+            ->add('couleur',ChoiceType::class, [
+                'choices' => [
+                    'Marron' => 'Marron',
+                    'Gris' => 'Gris',
+                    'Beige' => 'Beige',
+                    "Bleu" => "Bleu",
+                    
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'required' => false,
+                'label' => false
+            ])
+
+            ->add('dimentions',ChoiceType::class, [
+                'choices' => [
+                    '160' => '160',
+                    '180' => '180',
+                    '200' => '200',
+                    
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'required' => false,
+                'label' => false
+            ])
+        
+            ->add('Email',EmailType::class, [
+               
+                ])
+            ->add('produit', EntityType::class, [
+                'class' => Produits::class,
+                'label' => false,
+                'choice_label' => 'nom',
+                'empty_data'=>'',
+                'required'=>false,
+         
+            ])
+
         ;
     }
 
