fc92488422056b9696aebe3fec7ffa853f131b03
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-22 13:09:09 +0100

    update

--- src/Form/CommandesType.php
+++ src/Form/CommandesType.php
@@ -6,6 +6,18 @@ use App\Entity\Commandes;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class CommandesType extends AbstractType
 {
