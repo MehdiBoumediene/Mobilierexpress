98aeda7bd1c4a1922cdf2375d7d6ef5c6543bb8b
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-25 12:43:40 +0100

    modif

--- src/Form/CommandesType.php
+++ src/Form/CommandesType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Doctrine\ORM\EntityRepository;
