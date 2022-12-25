779770292e4c23c96b5d2b70f492ee619abca509
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-25 16:05:31 +0100

    modif

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -66,7 +66,7 @@ class IndexController extends AbstractController
     }
 
     #[Route('algerie/magasin/meuble/{categorie}/{id}', name: 'app_produit_details', methods: ['GET'])]
-    public function show($categorie,Produits $produit): Response
+    public function show($categorie,$id): Response
     {
         $produit = $produitsRepository->findOneBy(array('nom'=>$id));
         return $this->render('index/show.html.twig', [
