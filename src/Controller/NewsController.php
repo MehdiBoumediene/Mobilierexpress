94644f5e31765869f190c23cac742d8d45872838
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-29 22:06:53 +0100

    modif

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/news')]
+#[Route('/admin/news')]
 class NewsController extends AbstractController
 {
     #[Route('/', name: 'app_news_index', methods: ['GET'])]
