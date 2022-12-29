acff488f7beccafc8eb072a9e75ad3b034d9e742
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-29 22:32:16 +0100

    modif

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -3,6 +3,16 @@
 namespace App\Form;
 
 use App\Entity\News;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use PhpParser\Node\Stmt\Label;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +23,25 @@ class NewsType extends AbstractType
     {
         $builder
             ->add('titre')
-            ->add('description')
-            ->add('created_at')
+            ->add('description',CkEditorType::class, [
+
+                'label' => false,
+                ])
+                ->add('files',FileType::class,[
+                    'label' => false,
+                    'multiple' => true,
+                    'mapped'=> false,
+                    'required'=> false,
+                    
+            
+                
+                ])
+            ->add('created_at',DateTimeType::class,[
+                'widget' => "single_text",
+                'format' => 'yyyy-MM-dd H:mm',
+                'data' => new \DateTime("now")
+            ])
+            
         ;
     }
 
